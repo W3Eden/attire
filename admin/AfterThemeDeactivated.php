9516e49d8130a28ad2154b95f5cee850557b87b3
Shafayat Alam <imsobuj@gmail.com>
2023-04-05 13:53:48 +0600

    deactivate feedback form

--- admin/AfterThemeDeactivated.php
+++ admin/AfterThemeDeactivated.php
@@ -1,10 +1,15 @@
 <?php
-function wpdocs_selectively_enqueue_admin_script( $hook ) {
+add_action( 'admin_enqueue_scripts', 'attire_uninstall_feedback' );
+add_action( 'wp_ajax_theme_switch_reason', 'attire_send_feedback' );
+
+function attire_uninstall_feedback( $hook ) {
 	if ( 'themes.php' != $hook ) {
 		return;
 	}
 	wp_enqueue_script( 'jquery' );
-	wp_enqueue_script( 'my-script', get_template_directory_uri() . '/admin/my-script.js', array( 'jquery' ), '1.0', true );
+	wp_enqueue_script( 'attire-feedback', get_template_directory_uri() . '/admin/js/feedback.js', array( 'jquery' ), '1.0', true );
 }
 
-add_action( 'admin_enqueue_scripts', 'wpdocs_selectively_enqueue_admin_script' );
+function attire_send_feedback() {
+	
+}
--- admin/js/feedback.js
+++ admin/js/feedback.js
@@ -7,7 +7,6 @@
             let url = $(_this).prop('href');
 
             // Display a modal with a form asking for the reason for the switch
-            // Display a modal with a form asking for the reason for the switch
             var modalHtml = '<style>#theme-switch-modal {\n' +
                 '  background-color: rgba(0, 0, 0, 0.4);\n' +
                 '  display: none;\n' +
@@ -52,13 +51,16 @@
                 '  margin: 20px auto\n</style><div id="theme-switch-modal">';
             modalHtml += '<div class="theme-switch-modal-content">';
             modalHtml += '<form id="theme-switch-form">';
-            modalHtml += '<label><input type="radio" name="switch-reason" value="design"> Design change</label><br>';
-            modalHtml += '<label><input type="radio" name="switch-reason" value="performance"> Performance improvement</label><br>';
-            modalHtml += '<label><input type="radio" name="switch-reason" value="compatibility"> Compatibility issue</label><br>';
-            modalHtml += '<label><input type="radio" name="switch-reason" value="other"> Other reason</label><br>';
-            modalHtml += '<label for="switch-description">Description:</label>';
+            modalHtml += '<label><input type="radio" name="switch-reason" value="better"> I found a better theme.</label><br>';
+            modalHtml += '<label><input type="radio" name="switch-reason" value="fse"> I prefer Full Site Editing(FSE) themes.</label><br>';
+            modalHtml += '<label><input type="radio" name="switch-reason" value="design"> Design Issue.</label><br>';
+            modalHtml += '<label><input type="radio" name="switch-reason" value="performance"> Performance Issue.</label><br>';
+            modalHtml += '<label><input type="radio" name="switch-reason" value="compatibility"> Compatibility Issue.</label><br>';
+            modalHtml += '<label><input type="radio" name="switch-reason" value="other"> Other reason.</label><br>';
+            modalHtml += '<label for="switch-description">Please describe the issue in few sentences:</label>';
             modalHtml += '<textarea name="switch-description"></textarea>';
             modalHtml += '<button type="submit">Submit</button>';
+            modalHtml += '<button type="button">Skip</button>';
             modalHtml += '</form>';
             modalHtml += '</div>';
             modalHtml += '</div>';
